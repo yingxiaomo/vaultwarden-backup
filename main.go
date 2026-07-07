c528bf5fe6136f2059c10a4c4b5f142fd6a2fb94
YingXiaoMo <yingxiaomoya@gmail.com>
2026-07-07 23:11:12 +0800

    fix: Rclone SDK 配置路径需通过 config.SetConfigPath 设置

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ import (
 	_ "github.com/rclone/rclone/backend/webdav"
 	_ "github.com/rclone/rclone/backend/yandex"
 	"github.com/rclone/rclone/fs"
+	"github.com/rclone/rclone/fs/config"
 	"github.com/rclone/rclone/fs/config/configfile"
 	"github.com/rclone/rclone/fs/operations"
 	"github.com/yeka/zip"
@@ -139,6 +140,9 @@ var rcloneOnce sync.Once
 // ensureRcloneConfig 确保 rclone 配置已加载，只执行一次
 func ensureRcloneConfig() {
 	rcloneOnce.Do(func() {
+		if cfgPath := os.Getenv("RCLONE_CONFIG"); cfgPath != "" {
+			_ = config.SetConfigPath(cfgPath)
+		}
 		configfile.Install()
 	})
 }
